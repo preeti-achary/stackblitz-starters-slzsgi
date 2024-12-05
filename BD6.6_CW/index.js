ec7d269652ae6f69beffc5d8563b71a8ab05dd37
preeti-achary <git@stackblitz.com>
2024-12-05 23:19:51 +0530

    Initial commit

---
+++ BD6.6_CW/index.js
@@ -0,0 +1,23 @@
+const cors = require('cors');
+const express = require('express');
+const { getAllEmployees, getEmployeesById } = require('./controllers');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/employees', async (req, res) => {
+  const employees = getAllEmployees();
+  res.json({ employees });
+});
+
+app.get('/employees/details/:id', async (req, res) => {
+  let employee = getEmployeesById(parseInt(req.params.id));
+
+  res.json({
+    employee,
+  });
+});
+
+module.exports = { app };
---
+++ BD6.6_CW/tests/api.test.js
@@ -0,0 +1,108 @@
+const request = require('supertest');
+const { getAllEmployees } = require('../controllers');
+const { app } = require("../index.js")
+const http = require('http');
+const { describe } = require('node:test');
+
+jest.mock("../controllers", () => ({
+  ...jest.requireActual("../controllers"),
+  getAllEmployees: jest.fn(),
+}))
+
+let server;
+
+beforeAll(async() => {
+  server = http.createServer(app);
+  server.listen(3001);
+});
+
+afterAll( async () => {
+  server.close();
+});
+
+describe("Controller Function tests", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should return all employees", () => {
+    let mockedeEmployees = [
+      {
+          employeeId: 1,
+          name: 'Rahul Sharma',
+          email: 'rahul.sharma@example.com',
+          departmentId: 1,
+          roleId: 1,
+      },
+      {
+          employeeId: 2,
+          name: 'Priya Singh',
+          email: 'priya.singh@example.com',
+          departmentId: 2,
+          roleId: 2,
+      },
+      {
+          employeeId: 3,
+          name: 'Ankit Verma',
+          email: 'ankit.verma@example.com',
+          departmentId: 1,
+          roleId: 3,
+      },
+    ];
+
+    getAllEmployees.mockReturnValue(mockedeEmployees);
+    let result = getAllEmployees();
+    expect(result)toEqual(mockedeEmployees);
+    expect(result.length).toBe(3);
+  });
+});
+
+describe("API Endpoint test", () => {
+  it("GET /employees should get all employees", async () => {
+    const res = await request(server).get("/employees");
+    expect(res.status)toBe(200);
+    expect(res.body).toEqual({
+         employees : [
+        {
+            employeeId: 1,
+            name: 'Rahul Sharma',
+            email: 'rahul.sharma@example.com',
+            departmentId: 1,
+            roleId: 1,
+        },
+        {
+            employeeId: 2,
+            name: 'Priya Singh',
+            email: 'priya.singh@example.com',
+            departmentId: 2,
+            roleId: 2,
+        },
+        {
+            employeeId: 3,
+            name: 'Ankit Verma',
+            email: 'ankit.verma@example.com',
+            departmentId: 1,
+            roleId: 3,
+        },
+      ];
+    });
+    expect(res.body.employees.length)toBe(3);
+  });
+
+  it("GET /employees/details/:id should get an employee by ID", async () => {
+    const res = await request(server).get("/employees/details/1");
+    expect(res.status)toBe(200);
+    expect(res.body)toEqual({
+      employee: {
+        employeeId: 1,
+            name: 'Rahul Sharma',
+            email: 'rahul.sharma@example.com',
+            departmentId: 1,
+            roleId: 1,
+      },
+    });
+  });
+});
+
+
+
